71979d8d70bf7474cb32fcf740519d3f40509b38
charlesjamb <charles.jambon@gmail.com>
2016-12-16 15:10:43 -0500

    Immutable Data And Pure Rendering

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner.jsx';
 import Vote from './Vote.jsx';
 
 export default React.createClass({
+	mixins: [PureRenderMixin],
 	render: function() {
 		return (
 			<div>
