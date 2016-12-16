2f33bac63737a35f1e38d650f0fd458aa1165ab6
charlesjamb <charles.jambon@gmail.com>
2016-12-16 12:42:55 -0500

    Writing the UI for the Voting Screen

---
+++ src/components/Voting.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Winner from './Winner.jsx';
+import Vote from './Vote.jsx';
+
+export default React.createClass({
+	render: function() {
+		return (
+			<div>
+				{this.props.winner ?
+					<Winner ref="winner" winner={this.props.winner}/> :
+					<Vote {...this.props}/>}
+			</div>
+		);
+	}
+})
