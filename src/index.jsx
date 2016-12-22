4b944e739fbd498b932f3c33cd45613557ca53df
charlesjamb <charles.jambon@gmail.com>
2016-12-22 14:25:32 +0100

    setting up the socket.io client

--- src/index.jsx
+++ src/index.jsx
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducer from './reducer.js';
+import io from 'socket.io-client';
 
+import reducer from './reducer.js';
 import App from './components/App.jsx';
 import {VotingContainer} from './components/Voting.jsx';
 import {ResultsContainer} from './components/Results.jsx';
 
 const store = createStore(reducer);
-store.dispatch({
-	type: 'SET_STATE',
-	state: {
-		vote: {
-			pair: ['Sunshine', '28 Days Later'],
-			tally: {Sunshine: 2}
-		}
-	}
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => 
+	store.dispatch({type: 'SET_STATE', state})
+);
 
 const routes = <Route component={App}>
 	<Route path="/results" component={ResultsContainer}/>
@@ -27,7 +24,7 @@ const routes = <Route component={App}>
 
 ReactDOM.render(
 	<Provider store={store}>
-		<Router history={hashHistory}>{routes}</Router>,
-	</Provider>
+		<Router history={hashHistory}>{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 );
